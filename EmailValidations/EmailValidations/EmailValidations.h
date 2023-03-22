433b25c24229518a66e471143a982a7fa31d74bc
Subhash <subh21dec@gmail.com>
2023-03-23 01:13:35 +0530

    framework added

---
+++ EmailValidations/EmailValidations/EmailValidations.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidations.h
+//  EmailValidations
+//
+//  Created by Rana on 23/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidations.
+FOUNDATION_EXPORT double EmailValidationsVersionNumber;
+
+//! Project version string for EmailValidations.
+FOUNDATION_EXPORT const unsigned char EmailValidationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidations/PublicHeader.h>
+
+
